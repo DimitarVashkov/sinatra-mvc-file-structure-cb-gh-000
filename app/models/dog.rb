bcbefc37b3154cd8274110bcac5ae6a4ba950475
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 15:05:47 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,12 @@ class Dog
 attr_accessor :name, :age
 attr_reader :breed
 
+@@all = []
 def initialize(name,breed,age)
   @name = name
   @breed = breed
   @age = age
+  @@all << self
 end
 
 def self.all?
